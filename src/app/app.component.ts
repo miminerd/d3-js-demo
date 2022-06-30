665fecc191ac156ef99b54ea2df8daa6c0e43efc
Mimi <miminerd06@gmail.com>
2022-06-30 08:39:31 +0100

    add bulma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import define from '../../library/index.js';
- import {Runtime, Inspector} from '../../library/runtime.js';
+import { Component, OnInit } from "@angular/core";
+import define from "../../library/index.js";
+import { Runtime, Inspector } from "../../library/runtime.js";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  title = 'd3-demo';
-
-  constructor(){
+  title = "d3-demo";
+  constructor() {
     const runtime = new Runtime();
     const main = runtime.module(define, Inspector.into(document.body));
   }
   ngOnInit(): void {
-    console.log('not empty rule, testing prehook');
+    console.log("no empty hooks rule");
   }
 }
